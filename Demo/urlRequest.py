af9781100819e9a1cc75b0d03a08c6c325c21bae
yangdx <yangdongxian@gmail.com>
2014-08-28 10:28:18 +0800

    Signed-off-by: yangdx <yangdongxian@gmail.com>

--- Demo/urlRequest.py
+++ Demo/urlRequest.py
@@ -6,7 +6,7 @@ Created on 2014-8-22
 
 @auther: Eastsheen
 
-email:yangdongxian@gmail.com
+QQ:393857608
 
 '''
 
@@ -25,12 +25,11 @@ class url_request():
         self.headlers = {'User-Agent' : 'Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 4 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19'}
      
     #发送post请求     
-    def sendPost(self,marketUrl,fileSuc,fileFai,fileOth,fileError):
+    def sendPost(self,urls,fileSuc,fileFai):
         ErrorCount = 0
         passCount = 0
-        for read in marketUrl:     
+        for read in urls:     
             try:
-                #time.sleep(0.5)
                 code = urllib2.Request(url=read,headers=self.headlers)
                 urlCode = urllib2.urlopen(code)
                 urlread = urlCode.read()
@@ -39,35 +38,21 @@ class url_request():
                     codeUrl = str(urlCode.getcode()) + ',' + read
                     fileSuc.write(codeUrl)
                     passCount += 1
-                elif urlCode.getcode() == 400 or urlCode.getcode() == 404 or urlCode.getcode() == 403:
-                    codeUrl = str(urlCode.getcode()) + ',' + read
-                    fileFai.write(codeUrl)
-                elif str(urlCode.getcode()) == '404 Not Found':
-                    print urlCode.getcode()
-                else:
-                    codeUrl = str(urlCode.getcode()) + ',' + read
-                    fileOth.write(codeUrl)
             except Exception,e:
                 ErrorCount += 1
                 codeUrl = str(e)+','+read
-                fileError.write(codeUrl)              
-                #if e.getcode() == 403 or e.getcode() == 403:
-                    #codeUrl = str(urlCode.getcode()) + ',' + read
-                    #fileFai.write(codeUrl)
-                #else:
-                    #codeUrl = str(e)+','+read
-                    #fileError.write(codeUrl)
-        print 'ErrorCount:%s' % ErrorCount
-        print 'PassCount:%s' % passCount
+                fileFai.write(codeUrl)
+        print 'ErrorCount:%d PassCount:%s' % (ErrorCount,passCount)
 
     #发送get请求
     def send_get(self,urls,fileSuc,fileFai):
         Passcount = 0
-        ErrorCount = 0
-        try:
-            #设置模拟模式
-            self.set_ios_mobile()
-            while 1:
+        Failcount = 0
+        #设置模拟模式
+        self.set_ios_mobile()
+        #while循环从txt文件读取内容，判断not readline跳出while
+        while 1:
+            try:
                 readline = urls.readline()
                 if not readline:
                     break
@@ -77,17 +62,19 @@ class url_request():
                     mycode = urllib2.urlopen(my_request)
                     #url对象用read()方法读取response内容
                     myReadCode= mycode.read()
-                    #print "code:%s" % mycode
-                    print "getcodes:%s" % mycode.getcode()
-                    print "geturl:%s" % mycode.geturl()
-                    #print "info:%s" % mycode.info()
-                    #print "getcontent:%s" % myReadCode
-                    #write suc
-                    log = str(mycode.getcode())+mycode.geturl()
-                    fileSuc.write(log)
-        except Exception,e:
-            print e
-            #fileFai.write(e)
+                    #输入getcode()/geturl()/info()/read()相关信息
+                    #print "getcode:%s geturl:%s info:%s read:%s" % (mycode.getcode(),mycode.geturl(),mycode.info(),myReadCode)
+                    if mycode.getcode() == 200:
+                        log = str(mycode.getcode()) + ',' + mycode.geturl()
+                        fileSuc.write(log+'\n')
+                        Passcount += 1
+            except Exception,e:
+                log = str(e) + ',' + readline +'\n'
+                #print log
+                fileFai.write(log)
+                Failcount += 1
+        print "Passcount:%d Failcount:%d" % (Passcount,Failcount)
+            
     
     #模拟android Nexus 4 手机
     def set_android_mobile(self):
@@ -118,17 +105,12 @@ if __name__=='__main__':
     fileSuc = open('urlSuc.txt','w')
     fileFai = open('urlFai.txt','w')
     fileOth = open('urlOth.txt','w')
-    fileTes = open('marketTes.txt','w')
-    fileError = open('fileError.txt','w')
     urls.seek(0)
-    #urls.readline()      
-    #myMarket.sendPost(marketUrl,fileSuc,fileFai,fileOth,fileError)
-    url = 'http://m.ctrip.com/market/AdMonitorService.aspx'
-    myRequest.send_get(urls,fileSuc,fileFai)
+    #post
+    myRequest.sendPost(urls,fileSuc,fileFai)
+    #get
+    #myRequest.send_get(urls,fileSuc,fileFai)
     myRequest.closeFile()
-    
-    #da = sys.stdin.read()
-    #print  'data %s' % da    
     
     
 
